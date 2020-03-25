ef6a47f505f50023af5cc92c725950130ab6ae84
yx-ren <yunxiang.r@gmail.com>
2020-03-25 22:54:56 +0800

    review code

--- src/logger/LogHelper.cpp
+++ src/logger/LogHelper.cpp
@@ -3,38 +3,24 @@
 
 LOG_LITE_NS_BEIGN
 
+std::map<LOG_LEVEL, std::string> LogHelper::mLevelString =
+{
+    {LOG_LEVEL_TRACE,   "TRACE"},
+    {LOG_LEVEL_DEBUG,   "DEBUG"},
+    {LOG_LEVEL_INFO,    "INFO"},
+    {LOG_LEVEL_WARN,    "WARN"},
+    {LOG_LEVEL_ERROR,   "ERROR"},
+    {LOG_LEVEL_FAILED,  "FAILED"},
+    {LOG_LEVEL_OFF,     "OFF"},
+};
+
 std::string LogHelper::levelToString(LOG_LEVEL level)
 {
-    std::string log_level("");
-    switch(level)
-    {
-        case LOG_LEVEL_TRACE:
-            log_level = "TRACE";
-            break;
-        case LOG_LEVEL_DEBUG:
-            log_level = "DEBUG";
-            break;
-        case LOG_LEVEL_INFO:
-            log_level = "INFO";
-            break;
-        case LOG_LEVEL_WARN:
-            log_level = "WARN";
-            break;
-        case LOG_LEVEL_ERROR:
-            log_level = "ERROR";
-            break;
-        case LOG_LEVEL_FAILED:
-            log_level = "FAILED";
-            break;
-        case LOG_LEVEL_OFF:
-            log_level = "OFF";
-            break;
-        default:
-            log_level = "UNKNOWN";
-            break;
-    }
+    const auto it = mLevelString.find(level);
+    if (it != mLevelString.end())
+        return it->second;
 
-    return log_level;
+    return "";
 }
 
 std::string LogHelper::modeToString(int mode)
--- src/logger/LogHelper.h
+++ src/logger/LogHelper.h
@@ -2,6 +2,7 @@
 #define __LOG_LITE_LOG_HELPER_H__
 
 #include <string>
+#include <map>
 #include <logger/common.h>
 #include <logger/LogConfig.h>
 
@@ -15,6 +16,9 @@ public:
     static std::string modeToString(int mode);
 
     static std::string configToString(LogConfig conf);
+
+private:
+    static std::map<LOG_LEVEL, std::string> mLevelString;
 };
 
 LOG_LITE_NS_END
